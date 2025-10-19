1854d9062ef08fa1b9e789eedd02f104c058d8cc
web100ry <valerij.ivanjuk@gmail.com>
2025-10-19 13:53:47 +0000

    add comment for manifect

--- Lessons2/__manifest__.py
+++ Lessons2/__manifest__.py
@@ -4,9 +4,15 @@
     'author': 'Valerii Ivaniuk',
     'website': 'https://www.vivaniuk.pp.ua/',
     'category': 'Customizations',
+    # тут перелік категорій odoo/odoo/addons/base/data/ir_module_category_data.xml
     'license': 'OPL-1',
-    'version': '18.0.1.1.0',
-    'description': "",
+    'version': '18.0.1.1.1',
+    #18.0.X.Y.Z
+    #18.0 odoo version
+    #X module number модель або представлення змінено
+    #Y release number додаються нові функції
+    #Z version number виправлення помилок
+    'description': "Даний модуль створено в рамках уроків ODOO Shool",
     'depends': [
         'base',
     ],
@@ -20,6 +26,23 @@
     'installable': True,
     'auto_install': False,
     'images': [
-        'static/description/logoCUA.png'
+        'static/description/icon.png',
     ],
+    'application': False,
+    # 'assets':'',
+    # Статичні файли
+    # 'installable': True,
+    # Можливість встановлення модулю через інтерфейс
+    # 'maintainers':'',
+    # Організація, яка підтримує модуль
+    # 'summary':'',
+    # Опис з ключовими словами для пошуку
+    # 'pre_init_hook':'',
+    # 'post_init_hook':'',
+    # 'uninstall_hook':'',
+    # запуск методів у відповідні моменти (перед, після встановленняЮ або деінсталяції модуля)'
+    # 'images':'',
+    # 'price': '',
+    # 'currency':'',
+
 }
