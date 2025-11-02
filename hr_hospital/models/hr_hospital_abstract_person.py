8059ba6a7eefbb0e75ed1b0d3561cdeaa034b324
web100ry <valerij.ivanjuk@gmail.com>
2025-11-02 14:00:13 +0000

    add validation fo phone, mail

--- hr_hospital/models/hr_hospital_abstract_person.py
+++ hr_hospital/models/hr_hospital_abstract_person.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api
 from datetime import date
+from odoo.exceptions import ValidationError
 import re
 
 class AbstractPerson(models.AbstractModel):
@@ -13,6 +14,7 @@ class AbstractPerson(models.AbstractModel):
 
     phone = fields.Char()
     email = fields.Char()
+
     gender = fields.Selection(
         selection=[('male', 'Male'), ('female', 'Female'),('other', 'Other')])
 
@@ -20,12 +22,23 @@ class AbstractPerson(models.AbstractModel):
     age = fields.Integer(string="Вік", compute="_compute_age", store=True, readonly=False)
     fullname = fields.Char(compute="_compute_fullname", store=True, readonly=False)
 
+    @api.constrains('phone')
+    def _check_phone_format(self):
+        for record in self:
+            if record.phone and not re.match(r'^\+380\d{9}$', record.phone):
+                raise ValidationError("Телефон має бути у форматі +380XXXXXXXXX")
+
+    @api.constrains('email')
+    def _check_email_format(self):
+        for record in self:
+            if record.email and not re.match(r"[^@]+@[^@]+\.[^@]+", record.email):
+                raise ValidationError("Невірний формат email")
+
     @api.depends('birth_date')
     def _compute_age(self):
         today = date.today()
         for record in self:
             if record.birth_date:
-                # Обчислення різниці років
                 years = today.year - record.birth_date.year
                 # Якщо день народження ще не настав у цьому році — відняти 1
                 if (today.month, today.day) < (record.birth_date.month, record.birth_date.day):
