2165d7172c6bac0b89dfa5b1d4c6fac4a91647c0
web100ry <valerij.ivanjuk@gmail.com>
2025-11-24 22:31:49 +0200

    test

--- hr_hospital/__manifest__.py
+++ hr_hospital/__manifest__.py
@@ -5,7 +5,7 @@
     'website': 'https://www.vivaniuk.pp.ua/',
     'category': 'Customizations',
     'license': 'LGPL-3',
-    'version': '18.0.5.3.1',
+    'version': '18.0.5.3.2',
     'depends': [
         'base',
     ],
