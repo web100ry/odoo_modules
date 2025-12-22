328e908b5c7885727527fb74a54f8fa2720aa010
web100ry <valerij.ivanjuk@gmail.com>
2025-12-22 16:46:19 +0200

    fix linters

--- hr_hospital/wizard/hr_hospital_doctor_report_wizard.py
+++ hr_hospital/wizard/hr_hospital_doctor_report_wizard.py
@@ -1,6 +1,7 @@
 from odoo import models, fields, _
 from odoo.exceptions import UserError
 
+
 class DoctorReportWizard(models.TransientModel):
     _name = 'hr.hospital.doctor.report.wizard'
     _description = 'Doctor Report Wizard'
@@ -16,15 +17,20 @@ class DoctorReportWizard(models.TransientModel):
         if not self.doctor_ids:
             raise UserError(_('Please select at least one doctor.'))
 
-        return self.env.ref('hr_hospital.action_report_doctor_card').report_action(self.doctor_ids)
+        return (self.env.ref('hr_hospital.action_report_doctor_card').
+                report_action(self.doctor_ids))
 
     def action_preview_report(self):
         self.ensure_one()
         if not self.doctor_ids:
             raise UserError(_('Please select at least one doctor.'))
 
-        action = self.env.ref('hr_hospital.action_report_doctor_card').read()[0]
+        action = self.env.ref(
+            'hr_hospital.action_report_doctor_card'
+        ).read()[0]
         action['report_type'] = 'qweb-html'
         # Pass the IDs to the action, similar to report_action()
-        action['context'] = dict(self.env.context, active_ids=self.doctor_ids.ids)
+        action['context'] = dict(
+            self.env.context, active_ids=self.doctor_ids.ids
+            )
         return action
